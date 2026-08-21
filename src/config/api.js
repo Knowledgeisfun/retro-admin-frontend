75c2872705fb193cf81a715c639857a6d0c390ac
Sanjay Vinod K <sanjayvinodk@gmail.com>
2026-08-22 02:11:12 +0530

    api.js updated

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 // src/config.js
 
 // Whenever you restart Localtunnel/Ngrok, you ONLY change this ONE line:
-//export const API_BASE_URL = "https://tired-bushes-divide.loca.lt/api"; 
+export const API_BASE_URL = "club-manager-production-a880.up.railway.app"; 
 
 // If you want to test locally again without tunnels, just swap the comments!
-export const API_BASE_URL = "club-manager-production-a880.up.railway.app";
+//export const API_BASE_URL = "http://localhost:8080/api";
