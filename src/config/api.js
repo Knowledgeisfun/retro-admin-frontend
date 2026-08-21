4ae248b1c65fcfea6623eb028edc7ea619278620
Sanjay Vinod K <sanjayvinodk@gmail.com>
2026-08-22 02:26:48 +0530

    updated api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,2 @@
-// src/config.js
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// src/config/api.js
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
